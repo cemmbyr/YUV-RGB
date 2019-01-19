1bc0717ed7dd3fac0fa85afa62aef3d80d764e68
cemmbyr <cembyr98@gmail.com>
2019-01-19 04:15:23 +0300

    onbirinciproje

---
+++ YazLab3/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace YazLab3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(171, 328);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Dosya Seç";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(345, 27);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(390, 429);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "4-2-0",
+            "4-2-2",
+            "4-4-4"});
+            this.comboBox1.Location = new System.Drawing.Point(125, 36);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Formatı Seçiniz";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(125, 135);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(121, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(125, 233);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(121, 20);
+            this.textBox2.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 142);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Yükseklik";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 240);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Genişlik";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(16, 425);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 20);
+            this.label4.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(747, 480);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ YazLab3/Form1.cs
@@ -0,0 +1,232 @@
+﻿//CEM BAYIR 160202066
+//ÖZGÜR DEMİR 160202072
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace YazLab3
+{
+    public partial class Form1 : Form
+    {
+
+        public static int width;
+        public static int height;
+        public static int imgSize = width * height;
+        public static int frameSize = imgSize * 3 / 2;
+        public static int frameSayisi;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Interval = 50;
+            timer1.Start();
+            
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (Directory.Exists(@"C:\\Users\\ASUS\\source\\repos\\YazLab3\\resim"))
+            {
+                Directory.Delete(@"C:\\Users\\ASUS\\source\\repos\\YazLab3\\resim", true);
+
+            }
+            Directory.CreateDirectory(@"C:\\Users\\ASUS\\source\\repos\\YazLab3\\resim");
+
+
+            openFileDialog1.FileName = "Dosya Seçiniz";
+            openFileDialog1.Filter = "All files (*.*)|*.*";
+            openFileDialog1.Title = "Hızlı Resim";
+            openFileDialog1.InitialDirectory = "C:\\";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+                if (comboBox1.Text.Equals("") || textBox1.Text.Equals("") || textBox2.Text.Equals(""))
+                {
+                    label4.Text = "Bilgileri Eksiksiz Giriniz!";
+                }
+                else
+                {
+                label4.Text = "Yükleniyor....";
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                FileStream fs = File.OpenRead(openFileDialog1.FileName);
+                BinaryReader yeni = new BinaryReader(fs);
+                String format = comboBox1.Text;
+                height = Convert.ToInt32(textBox1.Text);
+                width = Convert.ToInt32(textBox2.Text);
+                Bitmap bm = new Bitmap(height, width);
+                
+                          
+                //4-2-0 Formatı
+                if (format.Equals("4-2-0"))
+                {
+                    int frameBoyut = height * width * 3 / 2;
+                    frameSayisi = (int)fs.Length / frameBoyut;
+                    int tampon, tmp = 0, tmp2 = 0, tmp3 = 0;
+                    int[] y = new int[width * height * frameSayisi];
+                    for (int i = 0; i < frameSayisi; i++)
+                    {
+
+                        for (int j = 0; j < height * width; j++)
+                        {
+                            tampon = fs.ReadByte();
+                            y[tmp] = (byte)tampon;
+                            tmp++;
+
+                        }
+                        for (int k = 0; k < width * height * 1 / 2; k++)
+                        {
+                            tampon = fs.ReadByte();
+                        }
+
+
+                    }
+                    for (int i = 0; i < frameSayisi; i++)
+                    {
+                        for (int j = 0; j < width; j++)
+                        {
+                            for (int k = 0; k < height; k++)
+                            {
+                                bm.SetPixel(k, j, Color.FromArgb(y[tmp2], y[tmp2], y[tmp2]));
+                                tmp2++;
+                            }
+
+                        }
+                        bm.Save("C:\\Users\\ASUS\\source\\repos\\YazLab3\\resim\\" + "gri" + tmp3 + ".bmp", ImageFormat.Bmp);
+                        tmp3++;
+
+                    }
+
+
+                }
+                //4-2-2 Formatı
+                if (format.Equals("4-2-2"))
+                {
+                    int frameBoyutu = height * width * 2;
+                    frameSayisi = (int)fs.Length / frameBoyutu;
+                    int tampon, tmp = 0, tmp2 = 0, tmp3 = 0;
+                    int[] y = new int[width * height * frameSayisi];
+                    for (int i = 0; i < frameSayisi; i++)
+                    {
+
+                        for (int j = 0; j < width * height; j++)
+                        {
+                            tampon = fs.ReadByte();
+
+                            y[tmp] = (byte)tampon;
+                            tmp++;
+
+                        }
+                        for (int k = 0; k < width * height * 1; k++)
+                        {
+                            tampon = fs.ReadByte();
+                        }
+
+                    }
+                    for (int i = 0; i < frameSayisi; i++)
+                    {
+                        for (int J = 0; J < width; J++)
+                        {
+                            for (int k = 0; k < height; k++)
+                            {
+
+                                bm.SetPixel(k, J, Color.FromArgb(y[tmp2], y[tmp2], y[tmp2]));
+                                tmp2++;
+
+                            }
+                        }
+                        bm.Save("C:\\Users\\ASUS\\source\\repos\\YazLab3\\resim\\" + "gri" + tmp3 + ".bmp", ImageFormat.Bmp);
+                        tmp3++;
+                    }
+
+
+                }
+                //4-4-4 Formatı
+                if (format.Equals("4-4-4"))
+                {
+                    int frameBoyutu = height * width * 3;
+                    frameSayisi = (int)fs.Length / frameBoyutu;
+                    int tampon, tmp = 0, tmp2 = 0, tmp3 = 0;
+                    int[] y = new int[width * height * frameSayisi];
+                    for (int i = 0; i < frameSayisi; i++)
+                    {
+
+                        for (int j = 0; j < width * height; j++)
+                        {
+                            tampon = fs.ReadByte();
+                            y[tmp] = (byte)tampon;
+                            tmp++;
+
+                        }
+                        for (int k = 0; k < width * height * 2; k++)
+                        {
+                            tampon = fs.ReadByte();
+                        }
+
+                    }
+                    for (int i = 0; i < frameSayisi; i++)
+                    {
+                        for (int J = 0; J < width; J++)
+                        {
+                            for (int k = 0; k < height; k++)
+                            {
+
+                                bm.SetPixel(k, J, Color.FromArgb(y[tmp2], y[tmp2], y[tmp2]));
+                                tmp2++;
+
+                            }
+                        }
+                        bm.Save("C:\\Users\\ASUS\\source\\repos\\YazLab3\\resim\\" + "gri" + tmp3 + ".bmp", ImageFormat.Bmp);
+                        tmp3++;
+                    }
+
+                }
+            }
+
+        }
+            
+
+
+
+        }
+        private void basla(int i)
+        {
+                
+            if (File.Exists("C:\\Users\\ASUS\\source\\repos\\YazLab3\\resim\\gri0.bmp") ==true)
+            {
+                pictureBox1.Refresh();
+                pictureBox1.Image = new Bitmap("C:\\Users\\ASUS\\source\\repos\\YazLab3\\resim\\" + "gri" + i + ".bmp");
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+                label4.Text = "";
+
+            }
+        }
+
+        public static int tanp = 0;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            
+            if (tanp > frameSayisi-1) {
+                tanp = 1;
+            }
+            basla(tanp);
+            tanp++;
+
+            timer1.Start();
+            
+        }
+    }
+}
